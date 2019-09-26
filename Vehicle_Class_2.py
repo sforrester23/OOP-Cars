c34506e32eb4b51932fd89695420eb22b77f18ba
Sam Forrester <SForrester@spartaglobal.com>
2019-09-26 15:42:45 +0100

    created class for vehicle_2: code along task

---
+++ Vehicle_Class_2.py
@@ -0,0 +1,12 @@
+class Vehicle2(_):
+
+    def __init__(self, wheels, capacity, colour):
+        self.wheels = wheels
+        self.capacity = capacity
+        self.colour = colour
+
+    def accelerate(self):
+        return 'VROOOOOOM'
+    def make_sound(self):
+        return 'MAKING NOISESSEEE BLEHAH'
+    

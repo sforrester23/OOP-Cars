d5675881fdd346adf894fb1a7c22cc257b8895bc
Sam Forrester <SForrester@spartaglobal.com>
2019-09-26 16:25:22 +0100

    more mucking about with classes and practising/learning Encapsulation methods for OOP

--- Car_Class_2.py
+++ Car_Class_2.py
@@ -1,11 +1,13 @@
 from Vehicle_Class_2 import *
 
 class Car2(Vehicle2):
-    def __init__(self, wheels, capacity, colour):
-        super().__init__(wheels, capacity, colour, make, model, plate)
+    def __init__(self, wheels, capacity, colour,  make, model, plate):
+        super().__init__(wheels, capacity, colour)
         self.make = make
         self.model = model
         self.plate = plate
+        self._accidents = 0 # Visibility is limited
+        self.__miles = 1000000 # Visibility and Access is restricted
 
 
     def make_sound(self):
@@ -13,4 +15,11 @@ class Car2(Vehicle2):
 
     def play_music(self, song):
         return 'PLAY: ' + song
+    
+    def show_miles(self):
+        return self.__miles
+
+    def set_miles(self, miles):
+        self.__miles = miles
+        return 'miles set to {} miles'.format(miles)
     
--- Vehicle_Class_Run.py
+++ Vehicle_Class_Run.py
@@ -2,30 +2,40 @@ from Vehicle_Class import *
 from Car_Class import *
 from Bicycle_Class import *
 from Car_Class_2 import *
+#
+# vehicle_1 = Vehicle(4, 5, 'Blue', 1989)
+#
+# car_1 = Car(4, 5, 'Grey', 2019, 'Hyundai', 'i30', 'GY59 GYK', True)
+#
+# print(car_1.license_plate)
+# print("Wow! That's a super nice {} {} {} {} with {} doors and {} person capacity.".format(car_1.colour, car_1.year, car_1.brand, car_1.model, car_1.num_wheels, car_1.capacity))
+#
+# bicycle_1 = Bicycle(2, 1, 'Midnight Grey', 2001, 'normal', 21, True)
+#
+# if bicycle_1.basket == False:
+#     print('That is a lovely {} {} bicycle with {} handlebars and {} gears but no basket'.format(bicycle_1.colour, bicycle_1.year, bicycle_1.handle_bar, bicycle_1.gears, bicycle_1.basket))
+# else:
+#     print('That is a lovely {} {} bicycle with {} handlebars and {} gears and a basket.'.format(bicycle_1.colour, bicycle_1.year, bicycle_1.handle_bar, bicycle_1.gears, bicycle_1.basket))
 
-vehicle_1 = Vehicle(4, 5, 'Blue', 1989)
 
-car_1 = Car(4, 5, 'Grey', 2019, 'Hyundai', 'i30', 'GY59 GYK', True)
+car_example = Car2(4,5,'green', 'Volvo', 'XC90', 'XOXOX')
+# print('Proof of inheritance:')
+# print(car_example)
+# print(car_example.make_sound()) # Inheriting the same methods as Vehicle class is INHERITANCE
+#
+# print('/////////////////////////////////')
+#
+# vehicle_example = Vehicle2(2,4, 'Blue')
+# print('Proof of method polymorphism with make_sound():')
+# print(vehicle_example.make_sound())
+# print(car_example.make_sound()) # METHOD POLYMORPHISM displayed by the same method being called on a parent class and its sub class but being different because it is defined wrong
+#
 
-print(car_1.license_plate)
-print("Wow! That's a super nice {} {} {} {} with {} doors and {} person capacity.".format(car_1.colour, car_1.year, car_1.brand, car_1.model, car_1.num_wheels, car_1.capacity))
 
-bicycle_1 = Bicycle(2, 1, 'Midnight Grey', 2001, 'normal', 21, True)
+print('Playing with Encapsulation')
+print(car_example.wheels)
+print(car_example._accidents)
+# print(car_example.__miles) # This will break, because it is a private/encapsulated figure
 
-if bicycle_1.basket == False:
-    print('That is a lovely {} {} bicycle with {} handlebars and {} gears but no basket'.format(bicycle_1.colour, bicycle_1.year, bicycle_1.handle_bar, bicycle_1.gears, bicycle_1.basket))
-else:
-    print('That is a lovely {} {} bicycle with {} handlebars and {} gears and a basket.'.format(bicycle_1.colour, bicycle_1.year, bicycle_1.handle_bar, bicycle_1.gears, bicycle_1.basket))
-
-
-
-print('Proof of inheritance:')
-print(Car2(4,5,'green'))
-print(Car2(4,5,'green').make_sound()) # Inheriting the same methods as Vehicle class is INHERITANCE
-
-print('/////////////////////////////////')
-
-print('Proof of method polymorphism with make_sound():')
-print(Vehicle2(2,4, 'Blue').make_sound())
-print(Car2(2,4,'Blue').make_sound()) # METHOD POLYMORPHISM displayed by the same method being called on a parent class and its sub class but being different because it is defined wrong
-
+print(car_example.set_miles(105))
+print(car_example.show_miles())
